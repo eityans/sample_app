5feabf919da6dcbaad1bce099457f769280ddee9
Eitaro Watanabe <eityans@gmail.com>
2019-06-10 03:59:25 +0000

    7-3

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,8 @@ Rails.application.routes.draw do
   
   get '/contact', to: 'static_pages#contact'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users
   
   
 end
